867dadf51555fbe874658d8e530e94814fcc754c
Lígia Andrade <andradeligiamp@gmail.com>
2024-11-14 11:31:27 -0300

    Construção do Component Poster

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -1,16 +1,18 @@
-import { Imagem, Logo, Texto } from "./styles"
+import { Bloco,  Imagem, Logo, Texto } from "./styles"
 import fundo from '../../assets/images/Fundo.png'
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo2.png'
 
 const Poster = () => (
     <Imagem style={{ backgroundImage: `url(${fundo})` }}>
         <div className='container'>
-        <Logo src={logo} width={125} height={57} alt="Ebac-Efood" /> 
-            <Texto>
+            <Bloco>
+            <Logo src={logo} width={125} height={58} alt="Ebac-Efood" /> 
+                <Texto>
             Viva experiências gastronômicas
             <br />
-                no conforto da sua casa
-            </Texto>            
+            no conforto da sua casa
+                </Texto> 
+            </Bloco>
             
         </div>
     </Imagem>
--- src/components/Poster/styles.ts
+++ src/components/Poster/styles.ts
@@ -3,24 +3,25 @@ import styled from "styled-components";
 
 export const Imagem = styled.div`
     width: 100%;
-    height: 384px;
+    height: 366px;
     display: block;
     background-repeat: no-repeat;
     background-size: cover;
+    margin-top: 0px;
     margin-bottom: 80px;
     `
-export const Logo = styled.img`
-    margin-top: 38px;
-    margin-left: 621px;
-    
+    export const Bloco = styled.div`
+    width: 539px;
+    max-width: 539px;
+    text-align: center;
+    margin-left: 240px;
     `
-    export const Texto = styled.p`
-        font-size: 36px;
-        font-weight: 900;
-        max-width: 539px;
-        margin: 138px 0px 0px;
-        display: block;
-        margin-block-start: 2em;
-        margin-block-end: 2em
-        
-`
+    export const Logo = styled.img`
+        margin-top: 40px;
+    `
+    
+    export const Texto = styled.h2`
+    margin-top: 125px;
+    font-size: 36px;
+    font-weight: bold;
+    `
